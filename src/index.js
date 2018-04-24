de056b8c84169f7fe2aee1a7a8a3e089c7d84130
= <beaumont_jonny@hotmail.co.uk>
2018-04-24 22:39:18 +0100

    Added display

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Button_1 = require("./models/Button");
+const Display_1 = require("./models/Display");
 const button = new Button_1.Button(26);
 button.waitForPress().then(() => {
     console.log('pressed!');
 });
+const display = new Display_1.Display({
+    rs: 11,
+    e: 9,
+    data: [10, 22, 27, 17],
+    cols: 16,
+    rows: 2
+});
+display.writeMessage(0, Display_1.Display.ROW.TOP, 'hello world').then(() => {
+    display.writeMessage(0, Display_1.Display.ROW.BOTTOM, new Date().toISOString().substring(11, 19));
+});
 process.on('SIGINT', function () {
     console.log("\nending!");
     button.closeButton();
+    display.closeLcd();
     process.exit();
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { Button } from "./models/Button";
+import { Display } from "./models/Display";
 
 const button: Button = new Button(26);
 button.waitForPress().then(() => {
     console.log('pressed!');
 });
 
+const display: Display = new Display({
+    rs: 11,
+    e: 9,
+    data: [10, 22, 27, 17],
+    cols: 16,
+    rows: 2
+});
+
+display.writeMessage(0, Display.ROW.TOP, 'hello world').then(() => {
+    display.writeMessage(0, Display.ROW.BOTTOM, new Date().toISOString().substring(11, 19))
+});
+
 process.on('SIGINT', function() {
     console.log("\nending!");
     button.closeButton();
+    display.closeLcd();
     process.exit();
 });
