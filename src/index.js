0b3b1f02b120516f2f3b21de768f93f8df38b9d8
= <beaumont_jonny@hotmail.co.uk>
2018-04-25 02:45:02 +0100

    Added bus stops 6

--- src/index.js
+++ src/index.js
@@ -53,13 +53,13 @@ function start2() {
                 yield display2.writeMessage(0, Display_1.Display.ROW.BOTTOM, message);
             }
             else if (deps.length === 1) {
-                message += `${deps[0].routeName} - ${deps[0].date} ${deps[0].estimatedDepartureTime}`;
+                message += `${deps[0].routeName} - ${deps[0].estimatedDepartureTime}`;
                 yield display2.writeMessage(0, Display_1.Display.ROW.BOTTOM, message);
             }
             else {
-                message += `${deps[0].routeName} - ${deps[0].date} ${deps[0].estimatedDepartureTime}`;
+                message += `${deps[0].routeName} - ${deps[0].estimatedDepartureTime}`;
                 yield display2.writeMessage(0, Display_1.Display.ROW.TOP, message);
-                message += `${deps[1].routeName} - ${deps[1].date} ${deps[1].estimatedDepartureTime}`;
+                message += `${deps[1].routeName} - ${deps[1].estimatedDepartureTime}`;
                 yield display2.writeMessage(0, Display_1.Display.ROW.BOTTOM, message);
             }
         }
